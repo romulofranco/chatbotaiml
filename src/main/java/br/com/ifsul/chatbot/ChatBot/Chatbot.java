3ba3f075e3624dd0eacccb80de13ac121bc96a40
Romulo Franco <romulojosefranco@gmail.com>
2018-09-30 20:53:27 -0300

    Encapsulamento dos metodos de obtencao de resposta inteligente

--- src/main/java/br/com/ifsul/chatbot/ChatBot/Chatbot.java
+++ src/main/java/br/com/ifsul/chatbot/ChatBot/Chatbot.java
@@ -6,58 +6,66 @@ import org.alicebot.ab.Chat;
 import org.alicebot.ab.History;
 import org.alicebot.ab.MagicBooleans;
 import org.alicebot.ab.MagicStrings;
-import org.alicebot.ab.utils.IOUtils;
 
 public class Chatbot {
-	private static final boolean TRACE_MODE = false;
-	static String botName = "super";
 
-	public static void main(String[] args) {
-		try {
+    private static final boolean TRACE_MODE = false;
+    static String botName = "super";
+    private String resourcesPath;
+    private Chat chatSession;
+    private Bot bot;
 
-			String resourcesPath = getResourcesPath();
-			System.out.println(resourcesPath);
-			MagicBooleans.trace_mode = TRACE_MODE;
-			Bot bot = new Bot("super", resourcesPath);
-			Chat chatSession = new Chat(bot);
-			bot.brain.nodeStats();
-			String textLine = "";
+    public Chatbot() {
+        resourcesPath = getResourcesPath();
+        MagicBooleans.trace_mode = TRACE_MODE;
+        bot = new Bot("super", resourcesPath);
+        chatSession = new Chat(bot);
+        bot.brain.nodeStats();
+    }
 
-			while (true) {
-				System.out.print("Human : ");
-				textLine = IOUtils.readInputTextLine();
-				if ((textLine == null) || (textLine.length() < 1))
-					textLine = MagicStrings.null_input;
-				if (textLine.equals("q")) {
-					System.exit(0);
-				} else if (textLine.equals("wq")) {
-					bot.writeQuit();
-					System.exit(0);
-				} else {
-					String request = textLine;
-					if (MagicBooleans.trace_mode)
-						System.out.println(
-								"STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0)
-										+ ":TOPIC=" + chatSession.predicates.get("topic"));
-					String response = chatSession.multisentenceRespond(request);
-					while (response.contains("&lt;"))
-						response = response.replace("&lt;", "<");
-					while (response.contains("&gt;"))
-						response = response.replace("&gt;", ">");
-					System.out.println("Robot : " + response);
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public String respostaInteligente(String textLine) {
+        try {
+            System.out.println(resourcesPath);
 
-	private static String getResourcesPath() {
-		File currDir = new File(".");
-		String path = currDir.getAbsolutePath();
-		path = path.substring(0, path.length() - 2);
-		System.out.println(path);
-		String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
-		return resourcesPath;
-	}
+            if ((textLine == null) || (textLine.length() < 1)) {
+                textLine = MagicStrings.null_input;
+            }
+            if (textLine.equals("q")) {
+                System.exit(0);
+                return "Bot foi encerrado";
+            } else if (textLine.equals("wq")) {
+                bot.writeQuit();
+                System.exit(0);
+                return "Bot foi encerrado";
+            } else {
+                String request = textLine;
+                if (MagicBooleans.trace_mode) {
+                    System.out.println(
+                            "STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0)
+                            + ":TOPIC=" + chatSession.predicates.get("topic"));
+                }
+                String response = chatSession.multisentenceRespond(request);
+                while (response.contains("&lt;")) {
+                    response = response.replace("&lt;", "<");
+                }
+                while (response.contains("&gt;")) {
+                    response = response.replace("&gt;", ">");
+                }
+                System.out.println("Robot : " + response);
+                
+                return response;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Falha";
+    }
+
+    private String getResourcesPath() {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        path = path.substring(0, path.length() - 2);
+        System.out.println(path);
+        return path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
+    }
 }
